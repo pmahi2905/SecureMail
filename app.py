16b0ed5e520990977f226b99dacdedcfb24253f6
Mahi Patel <mahi@Mahis-MacBook-Air.local>
2026-05-16 17:29:33 -0500

    Prepared project for deployment

--- app.py
+++ app.py
@@ -31,4 +31,9 @@ def history():
     return render_template("history.html", scans=scans)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
